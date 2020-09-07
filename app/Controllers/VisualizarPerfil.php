186830b4d24767442bfc0241db34fff0db756a33
Jim Padilla <haroldead.25@gmail.com>
2020-09-07 02:38:14 -0500

    Parte de visualizar perfil y visualizar usuario

--- app/Controllers/VisualizarPerfil.php
+++ app/Controllers/VisualizarPerfil.php
@@ -2,17 +2,18 @@
 namespace App\Controllers;
 use CodeIgniter\Controller;
 
-class VisualizarUsuario extends Controller
+class VisualizarPerfil extends Controller
 {
 	public function index()
 	{
 		$db = \Config\Database::connect();	
-		$model = $db -> query('SELECT p.nombre AS nombreperfil, u.idusuario, u.nombreusuario, u.nombre, u.dni, u.telefono,u.estado AS estadousuario, p.estado AS estadoperfil FROM perfil AS p INNER JOIN usuario AS u ON p.idperfil = u.idperfil');
+		$model = $db -> query('SELECT perf.idperfil, perf.nombre AS nombreperfil FROM perfil AS perf INNER JOIN permiso AS perm ON perf.idperfil = perm.idperfil INNER JOIN modulo AS m ON m.idmodulo = perm.idmodulo WHERE perm.estado = 1 AND perf.estado = 1 AND m.estado = 1 GROUP BY perf.idperfil, nombreperfil');
+		$model2 = $db -> query('SELECT perf.idperfil,  m.nombre AS nombremodulo FROM perfil AS perf INNER JOIN permiso AS perm ON perf.idperfil = perm.idperfil INNER JOIN modulo AS m ON m.idmodulo = perm.idmodulo WHERE perm.estado = 1 AND perf.estado = 1 AND m.estado = 1');
 		$datos["Resultado"] = $model -> getResultArray();
-
+		$datos["Resultado2"] = $model2 -> getResultArray();
 		echo view( 'header' );
 		echo view( 'menu' );
-		echo view( 'visualizar_usuario', $datos );
+		echo view( 'visualizar_perfil', $datos );
 		echo view( 'footer' );
 	}
 	public function update( $id )
--- app/Views/visualizar_perfil.php
+++ app/Views/visualizar_perfil.php
@@ -17,7 +17,7 @@
           <div class="">
             <div class="title">
               <div class="col-12">
-                <h3>Visualizar Usuarios</small></h3>
+                <h3>Visualizar Perfil</small></h3>
               </div>
             </div>
 
@@ -27,7 +27,7 @@
               <div class="col-md-12 col-sm-12 ">
                 <div class="x_panel ">
                   <div class="x_title">
-                    <a href="form_validation.php" class="btn btn-success">Registrar Usuario</a>
+                    <a href="form_validation.php" class="btn btn-success">Registrar Perfil</a>
                     <div class="clearfix"></div>
                   </div>
                   <div class="x_content">
@@ -39,35 +39,34 @@
                       <thead>
                         <tr>
                           <th>Id</th>
-                          <th>Nombre</th>
-                          <th>Nombre Usuario</th>
-                          <th>Teléfono/Celular</th>
                           <th>Perfil</th>
-                          <th>DNI</th>
+                          <th>Permisos</th>
                           <th>Acciones</th>
                         </tr>
                       </thead>
                       <tbody>
                         <?php  foreach ( $Resultado as $key => $value ):
-                          if ($value["estadoperfil"] == 1 && $value["estadousuario"] == 1) {
+                          //if ($value["estadoperfil"] == 1 && $value["estadousuario"] == 1) {
                           ?>
                         <tr>  
-                        <td> <?= $value[ "idusuario" ]   ?>  </td>
-                        <td> <?= $value[ "nombre" ] ?> </td>
-                        <td> <?= $value[ "nombreusuario" ] ?> </td>
-                        <td> <?= $value[ "telefono" ] ?> </td>
+                        <td> <?= $value[ "idperfil" ]   ?>  </td>
                         <td> <?= $value[ "nombreperfil" ] ?> </td>
-                        <td> <?= $value[ "dni" ] ?> </td>
+                        <td> <?php foreach ($Resultado2 as $key2 => $value2 ) 
+                        { 
+                          if ( $value2["idperfil"] == $value["idperfil"] ) {?> 
+                          <li> <?= $value2["nombremodulo"];?> </li> <?php }} ?>
+                      </td>
                         <td>
                           <div class="col-12 mx-auto px-0"> 
                             <div class="col-12 mx-auto text-align px-0">
-                            <a href="<?= base_url()."/index.php/visualizarusuario/update/".$value["idusuario"];?>" class="btn btn-info btn-sm mx-auto col-12" ><i class="fa fa-pencil"></i>Modificar</a></div>
+                            <a href="<?= base_url()."/index.php/visualizarperfil/update/".$value["idperfil"];?>" class="btn btn-info btn-sm mx-auto col-12" ><i class="fa fa-pencil"></i>Modificar</a></div>
                             <div class="col-12 mx-auto text-align px-0" >
-                            <a href="<?= base_url()."/index.php/visualizarusuario/delete/".$value['idusuario'];?>"  onclick="return confirm('¿Está seguro que desea eliminar este usuario?');" class="btn btn-danger btn-sm mx-auto col-12"><i class="fa fa-trash-o"></i>Eliminar</a>
+                            <a href="<?= base_url()."/index.php/visualizarperfil/delete/".$value['idperfil'];?>"  onclick="return confirm('¿Está seguro que desea eliminar este perfil?');" class="btn btn-danger btn-sm mx-auto col-12"><i class="fa fa-trash-o"></i>Eliminar</a>
                             </div>
-                            </div></td> 
+                            </div>
+                        </td> 
                         </tr>  
-                      <?php   } endforeach; ?>
+                      <?php endforeach; ?>
                       </tbody>
                     </table>
                   </div>
